0f701211b3594ab357b5da59b18b49c1a1a65cd0
valsam <mvsamaniego1@utpl.edu.ec>
2023-11-08 16:14:55 -0500

    guardar cambios

--- ejemplos/Ejemplo2/src/ejemplo2/Ejemplo3.java
+++ ejemplos/Ejemplo2/src/ejemplo2/Ejemplo3.java
@@ -30,7 +30,12 @@ public class Ejemplo3 {
         double nota1;
         double nota2;
         double suma;
-        
+        String miNombre;
+        String apellido;
+        System.out.println("Ingrese el nombre por favor");
+        miNombre = entrada.nextLine();
+        System.out.println("Ingrese su apellido por favor");
+        apellido = entrada.nextLine();
         System.out.println("Ingrese el nota 1, por favor");
         nota1 = entrada.nextDouble();
         System.out.println("Ingrese el nota 2, por favor");
@@ -39,11 +44,13 @@ public class Ejemplo3 {
         promedio = suma / 2;
         
         if (promedio >= 7.5) {
-            System.out.printf("Estudiante aprobado con un "
-                    + "promedio: %.2f\n", promedio);
+            System.out.printf("Estudiante %s %s, con notas %.2f y %.2f aprobado"
+                    +" con un promedio: %.2f\n",miNombre, apellido,
+                    nota1, nota2, promedio);
         }else{
-            System.out.printf("Estudiante reprobado con un "
-                    + "promedio: %.2f\n", promedio);
+            System.out.printf("Estudiante %s %s, con notas %.2f y %.2f reprobado"
+                    +"con un promedio: %.2f\n",miNombre, apellido,
+                    nota1, nota2, promedio);
         }
     }
     
